71e69cf05e95d2967a25373e5e1af5a837b0f91e
BananasAmIRite <jasony1230@gmail.com>
2023-02-04 10:27:54 -0500

    added default constructor to SplineWaypoint

--- src/main/java/org/bananasamirite/robotmotionprofile/data/waypoint/SplineWaypoint.java
+++ src/main/java/org/bananasamirite/robotmotionprofile/data/waypoint/SplineWaypoint.java
@@ -5,6 +5,8 @@ import org.bananasamirite.robotmotionprofile.Waypoint;
 
 public class SplineWaypoint extends Waypoint {
 
+    public SplineWaypoint() {}
+
     public SplineWaypoint(double x, double y, double angle, double weight, double runTime, TankMotionProfile.TankMotionProfileConstraints constraints) {
         super(x, y, angle, weight, runTime, constraints);
     }
