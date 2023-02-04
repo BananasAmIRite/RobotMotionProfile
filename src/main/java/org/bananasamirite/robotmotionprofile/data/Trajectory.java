1e9ffd22b56aaa6a5145b3c574bc99659128fd51
BananasAmIRite <jasony1230@gmail.com>
2023-02-03 21:34:50 -0500

    fixed bug with spline points not connecting with command points

--- src/main/java/org/bananasamirite/robotmotionprofile/data/Trajectory.java
+++ src/main/java/org/bananasamirite/robotmotionprofile/data/Trajectory.java
@@ -80,6 +80,7 @@ public class Trajectory {
         List<Waypoint> currentSplinePoints = new ArrayList<>(); 
         for (Waypoint w : waypoints) {
             if (w instanceof CommandWaypoint) {
+                currentSplinePoints.add(w);
                 WaypointTask t = new WaypointTask(currentSplinePoints, ProfileMethod.TIME, config.getConstraints()); 
                 tasks.add(t); 
                 tasks.add(new CommandTask((CommandWaypoint) w)); 
